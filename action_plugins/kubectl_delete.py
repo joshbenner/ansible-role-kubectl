b85da61cf60b1dfde8a4746c1b824d69e0718426
Elijah Lazkani <elijah@elazkani.com>
2018-01-08 23:18:49 -0500

    Add kubectl_delete * kubectl_delete only supports file, data or raw (for now) * README NOTE to prevent installing kubectl

---
+++ action_plugins/kubectl_delete.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import re
+
+from ansible.errors import AnsibleActionFail
+from ansible.plugins.action import ActionBase
+from ansible.module_utils.basic import jsonify
+
+
+class ActionModule(ActionBase):
+    out_re = re.compile(r'^(?P<kind>[^ ]+) "(?P<name>[^"]+)" (?P<action>.+)$')
+
+    def run(self, tmp=None, task_vars=None):
+        self._supports_check_mode = False
+        super(ActionModule, self).run(tmp, task_vars)
+
+        kubeconfig = self._task.args.get('kubeconfig', None)
+        context = self._task.args.get('context', None)
+        namespace = self._task.args.get('namespace', None)
+        filepath = self._task.args.get('file', None)
+        data = self._task.args.get('data', None)
+        raw = self._task.args.get('raw', None)
+
+        if kubeconfig is None:
+            raise AnsibleActionFail('kubeconfig is required')
+        if context is None:
+            raise AnsibleActionFail('context is required')
+
+        non_nulls = sum(int(c is not None) for c in (filepath, data, raw))
+        if non_nulls > 1:
+            raise AnsibleActionFail('Only one of file, data, or raw accepted')
+        elif non_nulls == 0:
+            raise AnsibleActionFail('file, data, or raw required')
+
+        if filepath is not None:
+            with open(filepath, 'r') as file_to_apply:
+                raw = file_to_apply.read()
+        elif data is not None:
+            raw = jsonify(data)
+
+        params = [
+            '--kubeconfig={}'.format(kubeconfig),
+            '--context={}'.format(context),
+            'delete',
+            '-f -'
+        ]
+        if namespace is not None:
+            params.append('--namespace={}'.format(namespace))
+
+        # Cannot abstract this further, as plugins cannot include from
+        # role-provided module utils like modules can.
+        module_return = self._execute_module(
+            module_name='command',
+            module_args=dict(
+                _raw_params="kubectl {}".format(' '.join(params)),
+                stdin=raw
+            )
+        )
+
+        results = []
+        unparsed_lines = []
+        changed = False
+        for line in module_return['stdout_lines']:
+            m = self.out_re.match(line)
+            if m:
+                results.append(m.groupdict())
+                if m.group('action') != 'unchanged':
+                    changed = True
+            else:
+                unparsed_lines.append(line)
+
+        module_return.update(dict(
+            changed=changed,
+            unparsed_lines=unparsed_lines,
+            results=results,
+            stdin=raw
+        ))
+        return module_return
