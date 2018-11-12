250854d0ed2787fbaf42a95c5ff0f68f46b63e34
Josh Benner <joshb@aweber.com>
2018-11-11 21:30:58 -0500

    Fix lint errors

--- molecule/default/tests/test_repo.py
+++ molecule/default/tests/test_repo.py
@@ -5,6 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('repo')
 
+
 def test_kubectl_installed(host):
     p = host.package('kubectl')
     assert p.is_installed
